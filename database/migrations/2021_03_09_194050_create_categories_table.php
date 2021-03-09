a399db7804b9dcb3dd2e4b4522bd6e51aca32284
cmunhozprog03h <cmunhozprog03@hotmail.com>
2021-03-09 19:51:35 -0300

    Create table Category, Model and Controller

--- database/migrations/2021_03_09_194050_create_categories_table.php
+++ database/migrations/2021_03_09_194050_create_categories_table.php
@@ -15,7 +15,14 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->string('description')->nullable();
+            $table->string('image')->nullable();
+            $table->integer('position');
+            $table->integer('active');
+            $table->string('slug');
             $table->timestamps();
+            
         });
     }
 
